fa8771197c6f741169a60ebbacfa322595bdc0d1
gabjo <gmorsaa532@g.educaand.es>
2026-01-15 19:19:56 +0100

    primer commit

---
+++ prueba01/script.js
@@ -0,0 +1,11 @@
+console.log("Hola Mundo");
+let num1 = prompt("Número 1:");
+let num2 = prompt("Número 2: ");
+
+
+
+function sumar(n1, n2){
+    return n1 * n2;
+}
+
+console.log(sumar(num1, num2));
